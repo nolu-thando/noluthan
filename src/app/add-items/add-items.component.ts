22b4c225e5f59927408d8c3eb173e8d4a2803ada
Noluthando <noluthandowendy2013@gmail.com>
2020-12-09 15:18:15 +0200

    New push

--- src/app/add-items/add-items.component.ts
+++ src/app/add-items/add-items.component.ts
@@ -1,4 +1,6 @@
 import { Component} from '@angular/core';
+import { CalculatorService } from '../calculator.service';
+import { Models } from '../models';
 
 @Component({
   selector: 'app-add-items',
@@ -7,27 +9,23 @@ import { Component} from '@angular/core';
 })
 
 export class AddItemsComponent {
-  income: number;
+  income: Models;
   expense: number;
   description: String;
   dateAdded:Date;
-  public rows:Array<{ name:string;
-    email:string;
-    id:number;
-    modules:string;
-    dateRegistered:Date;
-    dateModified:Date}> = [];
+  amount: number;
+  
+  
+ 
 // set local storage
  
-  constructor(){}
+  constructor(
+    private calculatorService: CalculatorService
+    ){}
   ngOnInit(): void {
-    // if (this.item) {
-    //   //if item has a value (existing item)
-    //   this.isNewItem = false
-    // } else {
-    //   this.isNewItem = true
-    //   //this.item = new ('', null)
-    // }
+   
+    this.income = this.calculatorService.addIncome(newIncome);
+
   }
 
   editField: string;
@@ -47,13 +45,13 @@ export class AddItemsComponent {
 
   add() {
     this.personList.push({ income: this.income, expense: this.expense, description: this.description, dateAdded: this.dateAdded});
-    if (this.personList.length > 0) {
-      const person = this.personList[0];
-      this.personList.push(person);
-      this.personList.splice(0, 1);
-      localStorage.setItem('personList',JSON.stringify(this.personList))
+    // if (this.personList.length > 0) {
+    //   const person = this.personList[0];
+    //   this.personList.push(person);
+    //   this.personList.splice(0, 1);
+    //   localStorage.setItem('personList',JSON.stringify(this.personList))
       console.log(this.personList);
-     }
+    // }
   }
 
   changeValue(id: number, property: string, event: any) {
